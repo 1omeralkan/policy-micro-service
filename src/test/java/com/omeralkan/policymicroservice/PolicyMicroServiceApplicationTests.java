170f5c572d9a3f3c00fc304e515f2d3e3828a357
Ömer Alkan <120298619+1omeralkan@users.noreply.github.com>
2026-05-04 00:24:27 +0300

    policy-micro-service projesi oluşturuldu.

---
+++ src/test/java/com/omeralkan/policymicroservice/PolicyMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeralkan.policymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
